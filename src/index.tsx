c50cdc8028951567e221ff1305434d35d65881f8
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-08-08 13:06:59 +0900

    Babelの設定変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import 'babel-polyfill';
 import { store } from './modules/store';
 import { App } from './components/App';
 
